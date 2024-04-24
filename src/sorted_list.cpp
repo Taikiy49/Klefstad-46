45888347f72a86fd3e89ebd777ec48e0672ae642
Taikiy49 <taikiy49@gmail.com>
2024-04-24 12:26:17 -0700

    done

--- src/sorted_list.cpp
+++ src/sorted_list.cpp
@@ -1,241 +1,367 @@
+
 #include <iostream>
+#include <string>
 #include <fstream>
 #include "sorted_list.h"
 #include "Timer.h"
+
 using namespace std;
 
-int SortedArrayList::find_index(const string & word){ // binary search!
-    int low = 0;
-    int high = size - 1; // consider the nullptr   
-    while (low <= high){ 
-        int mid = (low + high) / 2; // finding the mid will always be important for binary search!
-        if (word == buf[mid]) return mid;
-        else if (word < buf[mid]) high = mid - 1;
-        else low = mid + 1;}
-    return -1;}
-
-void SortedArrayList::copy_down(int hole){
-    auto _hole  = begin() + hole;
-    std::copy_backward(_hole, end(), end() + 1);
-    ++size;}
-
-void SortedArrayList::copy_up(int hole){
-    auto _hole = begin() + hole;
-    std::copy(_hole + 1, end(), _hole);
-    --size;
+SortedArrayList::SortedArrayList(int cap)
+    : SortedList("SortedArrayList"), capacity(cap), size(0), buf(new string[cap])
+{
 }
 
-// constructor!
-SortedArrayList::SortedArrayList(int cap):SortedList("ArrayList"), buf(new string[cap]), capacity(cap), size(0){}
-
-void SortedArrayList::insert(const string & word){
-    if (size==0){
-        buf[0]=word;
-        ++size; 
-        return;
-    }
-    int i=0;
-    for (auto e: *this){
-        if (word < e) break;
-        ++i;
-    }
-    copy_down(i);
-    buf[i] = word;
-}
-
-bool SortedArrayList::find(const string & word){
-    if (SortedArrayList::find_index(word)!= -1) return 1;
-    else return 0;
-}
-
-void SortedArrayList::remove(const string & word){
-    int i = SortedArrayList::find_index(word);
-    if (i!=-1 && i>=0 && i<size) copy_up(i);
-}
-
-bool SortedArrayList::is_empty(){
-    return size == 0;
-}
-
-bool SortedArrayList::is_full(){
-    return size == capacity;
-}
-
-void SortedArrayList::print(ostream &out){
-    for (auto e: *this)
-        out << e << ' ';
-}
-
-SortedArrayList::~SortedArrayList(){
+// Destructor
+SortedArrayList::~SortedArrayList()
+{
     delete[] buf;
 }
 
-//////////////////////////////////////////////
-
-void ListNode::print(ostream &out, ListNode *L){
-    out << L->data;
-}
-
-void ListNode::insert(const string & word, ListNode * & L){
-    ListNode *prev = nullptr;
-    for (ListNode *p=L; p!=nullptr; prev=p, p=p->next){
-        if (p->data>word){
-            ListNode *n = new ListNode(word, p);
-            if(prev == nullptr) L = n;
-            else prev->next = n;
-            return;
+// find_index helper
+int SortedArrayList::find_index(const string &word)
+{
+    int low = 0;         // lower bound
+    int high = size - 1; // upper bound
+    int mid = (low + high) / 2;
+    for (; low <= high; mid = (low + high) / 2)
+    { // repeat until the search range is empty
+        if (buf[mid] == word)
+        {               // if the middle element is the target word
+            return mid; // return its index
         }
-        else if(p->data==word) return;
-    }
-    ListNode *n = new ListNode(word, nullptr);
-    if (prev==nullptr) L=n;
-    else prev->next = n;
-    return;
-}
-
-
-ListNode * ListNode::find(const string &word, ListNode * L){
-    for(ListNode *p = L; p!=nullptr; p=p->next){
-        if (p->data == word){
-            return p;
+        else if (buf[mid] < word)
+        {                  // if the middle element is less than the target word
+            low = mid + 1; // update the lower bound
+        }
+        else
+        {                   // if the middle element is greater than the target word
+            high = mid - 1; // update the upper bound
         }
     }
-    return nullptr;
+    return low; // return the index
 }
 
-void ListNode::delete_list(ListNode *L){
-    ListNode * l = L;
-    for (;l!=nullptr;){
-        l = l->next;
-        delete L;
-        L = l;
+// copy_up helper
+void SortedArrayList::copy_up(int hole)
+{
+    for (int i = hole; i < size - 1; ++i)
+    {
+        buf[i] = buf[i + 1];
+    }
+    --size;
+}
+
+// copy_Down helper
+void SortedArrayList::copy_down(int hole)
+{
+    for (int i = size; i > hole; --i)
+    {
+        buf[i] = buf[i - 1];
+    }
+    ++size;
+}
+
+bool SortedArrayList::find(const string &item)
+{
+    int i = find_index(item);
+    return (i < size && buf[i] == item);
+}
+
+void SortedArrayList::insert(const string &item)
+{
+    if (is_full())
+    {
+        error("Opps! List is full", item);
+        cout << "size:" << size << endl;
+        cout << "capacity:" << capacity << endl;
+        exit(0);
+        return;
+    }
+    int i = find_index(item);
+    copy_down(i);
+    buf[i] = item;
+}
+
+void SortedArrayList::remove(const string &item)
+{
+    int i = find_index(item);
+    if (buf[i] == item)
+    {
+        copy_up(i);
     }
 }
 
-void ListNode::remove(const string &word, ListNode * & L){
-    ListNode *r = ListNode::find(word, L);
-    if (r==L){
-        L = r->next;
-        delete r;
+bool SortedArrayList::is_empty()
+{
+    return size == 0;
+}
+
+bool SortedArrayList::is_full()
+{
+    return size == capacity;
+}
+
+// Use itegrator to print
+void SortedArrayList::print(ostream &out)
+{
+    for (auto e : *this)
+    {
+        out << e << " ";
     }
-    else{
-        ListNode * prev = nullptr;
-        for (ListNode * p = L; p!=nullptr; prev = p, p=p->next){
-            if (p==r){
-                prev->next=p->next;
-                delete r;
-                return;
-            }
-        }
-    }
 }
 
-//////////////////////////////////////////
-
-SortedLinkedList::SortedLinkedList():SortedList("LinkedList"), head(nullptr){}
-
-void SortedLinkedList::insert(const string &word){
-    ListNode::insert(word, head);
-}
-
-bool SortedLinkedList::find(const string &word){
-    if (ListNode::find(word, head)) return -1;
-    else return 0;
-}
-
-void SortedLinkedList::remove(const string &word){
-    ListNode::remove(word, head);
-}
-
-bool SortedLinkedList::is_empty(){
-    return head == nullptr;
-}
-
-bool SortedLinkedList::is_full(){
-    return 0;
-}
-
-void SortedLinkedList::print(ostream &out){
-    for (auto e: *this)
-        out << e << ' ';
-}
-
-SortedLinkedList::~SortedLinkedList(){
-    ListNode::delete_list(head);
-}
-
-
-//////////////////
-
-ostream & operator<<(ostream &out, SortedList & L){
+ostream &operator<<(ostream &out, SortedList &L)
+{
     L.print(out);
     return out;
 }
 
-void error(string word, string msg)
+// SLL
+SortedLinkedList::SortedLinkedList() : SortedList("SortedLinkedList"), head(nullptr) {}
+
+void SortedLinkedList::insert(const string &word)
 {
-    std::cout << "ERROR: " << word << " " << msg << endl;
+    ListNode::insert(word, head);
 }
 
-void insert_all_words(int k, string file_name, SortedList & L)
+bool SortedLinkedList::find(const string &word)
 {
-    Timer t;
-    double eTime;
-    ifstream in(file_name);
-    int limit = k*NWORDS/10;
-    t.start();
-    for(string word; (in>>word)&& limit>0;--limit)
-        L.insert(word);
-    t.elapsedUserTime(eTime);
-    in.close();
-    std::cout<<"\t\tI = "<<eTime<<endl;
-
-}
-void find_all_words(int k, string file_name, SortedList & L)
-{
-    Timer t;
-    double eTime;
-    ifstream in(file_name);
-    int limit = k*NWORDS/10;
-    t.start();
-    for(string word; (in>>word)&& limit>0;--limit)
-        L.find(word);
-    t.elapsedUserTime(eTime);
-    in.close();
-    std::cout<<"\t\tI = "<<eTime<<endl;
+    return ListNode::find(word, head) != nullptr;
 }
 
-void remove_all_words(int k, string file_name, SortedList & L)
+void SortedLinkedList::remove(const string &word)
 {
-    Timer t;
-    double eTime;
-    ifstream in(file_name);
-    int limit = k*NWORDS/10;
-    t.start();
-    for(string word; (in>>word)&& limit>0;--limit)
-        L.remove(word);
-    t.elapsedUserTime(eTime);
-    in.close();
-    std::cout<<"\t\tI = "<<eTime<<endl;
+    ListNode::remove(word, head);
 }
 
-void measure_SortedList_methods(string file_name, SortedList & L)
+bool SortedLinkedList::is_empty()
 {
-    std::cout<<L.name<<endl;
-    for(int K=1; K<=10;++K)
+    return head == nullptr;
+}
+
+bool SortedLinkedList::is_full()
+{
+    return false;
+}
+
+void SortedLinkedList::print(ostream &out)
+{
+    for (auto e : *this)
     {
-        std::cout<<"\tK = "<<K<<endl;
-        insert_all_words(K,file_name,L);
-        find_all_words(K,file_name,L);
-        remove_all_words(K,file_name,L);
-        if(!L.is_empty())
-            error(L.name, "is not empty");
+        out << e << " ";
     }
 }
+
+void ListNode::insert(const string &word, ListNode *&L)
+{
+    ListNode *prev = nullptr;
+
+    for (ListNode *p = L; p != nullptr && word >= p->data; prev = p, p = p->next)
+    {
+        if (word == p->data)
+        {
+            return;
+        }
+    }
+    if (prev == nullptr)
+    {
+        L = new ListNode(word, L);
+    }
+    else
+    {
+        prev->next = new ListNode(word, prev->next);
+    }
+}
+
+ListNode *ListNode::find(const string &word, ListNode *L)
+{
+    while (L != nullptr && L->data != word)
+    {
+        L = L->next;
+    }
+    return L;
+}
+
+void ListNode::remove(const string &word, ListNode *&L)
+{
+    ListNode *prev = nullptr;
+    ListNode *curr = L;
+    while (curr != nullptr)
+    {
+        if (curr->data == word)
+        {
+            if (prev == nullptr)
+            {
+                L = curr->next;
+            }
+            else
+            {
+                prev->next = curr->next;
+            }
+            delete curr;
+            return;
+        }
+        prev = curr;
+        curr = curr->next;
+    }
+}
+
+void ListNode::delete_list(ListNode *L)
+{
+    if (L != nullptr)
+    {
+        delete_list(L->next);
+        delete L;
+    }
+}
+
+SortedLinkedList::iterator SortedLinkedList::begin()
+{
+    return iterator(head);
+}
+
+SortedLinkedList::iterator SortedLinkedList::end()
+{
+    return iterator(nullptr);
+}
+
+SortedLinkedList::iterator::iterator(ListNode *const ptr) : current(ptr) {}
+
+SortedLinkedList::iterator &SortedLinkedList::iterator::operator++()
+{
+    current = current->next;
+    return *this;
+}
+
+SortedLinkedList::iterator SortedLinkedList::iterator::operator++(int)
+{
+    iterator temp = *this;
+    ++(*this);
+    return temp;
+}
+
+string &SortedLinkedList::iterator::operator*() const
+{
+    return current->data;
+}
+
+string *SortedLinkedList::iterator::operator->() const
+{
+    return &(current->data);
+}
+
+bool SortedLinkedList::iterator::operator==(iterator const &other) const
+{
+    return current == other.current;
+}
+
+bool SortedLinkedList::iterator::operator!=(iterator const &other) const
+{
+    return !(*this == other);
+}
+
+SortedLinkedList::~SortedLinkedList()
+{
+    ListNode::delete_list(head);
+}
+
+// Helper methods
+void error(string word, string msg)
+{
+    cerr << "Error: " << msg << " - " << word << endl;
+}
+
+void insert_all_words(int k, string file_name, SortedList &L)
+{
+    Timer t;
+    double eTime;
+    ifstream infile(file_name);
+    if (!infile)
+    {
+        error("Unable to open input file", file_name);
+        return;
+    }
+    string word;
+    int limit = k * NWORDS / 10;
+    t.start();
+    for (int i = 0; i < limit && infile >> word; ++i)
+    {
+        L.insert(word);
+    }
+    t.elapsedUserTime(eTime);
+    infile.close();
+    cout << "\t\tI = " << eTime << endl;
+}
+
+void find_all_words(int k, string file_name, SortedList &L)
+{
+    Timer t;
+    double eTime;
+    ifstream infile(file_name);
+    if (!infile)
+    {
+        error("Unable to open input file", file_name);
+        return;
+    }
+    string word;
+    int limit = k * NWORDS / 10;
+    t.start();
+    for (int i = 0; i < limit; ++i)
+    {
+        infile >> word;
+        L.find(word);
+    }
+    t.elapsedUserTime(eTime);
+    infile.close();
+    cout << "\t\tF = " << eTime << endl;
+}
+
+void remove_all_words(int k, string file_name, SortedList &L)
+{
+    Timer t;
+    double eTime;
+    ifstream infile(file_name);
+    if (!infile)
+    {
+        error("Unable to open input file", file_name);
+        return;
+    }
+    string word;
+    int limit = k * NWORDS / 10;
+    t.start();
+    for (int i = 0; i < limit; ++i)
+    {
+        infile >> word;
+        L.remove(word);
+    }
+    t.elapsedUserTime(eTime);
+    infile.close();
+    cout << "\t\tR = " << eTime << endl;
+}
+
+void measure_SortedList_methods(string file_name, SortedList &L)
+{
+    cout << L.name << endl;
+    for (int K = 1; K <= 10; ++K)
+    {
+        cout << "\tK = " << K << endl;
+        insert_all_words(K, file_name, L);
+        find_all_words(K, file_name, L);
+        remove_all_words(K, file_name, L);
+        if (!L.is_empty())
+        {
+            error(L.name, "is not empty");
+            exit(0);
+        }
+    }
+}
+
 void measure_lists(string input_file)
 {
-    SortedArrayList UAL(NWORDS);
-    measure_SortedList_methods(input_file, UAL);
-    SortedLinkedList ULL;
-    measure_SortedList_methods(input_file, ULL);
+    SortedArrayList SAL(NWORDS);
+    measure_SortedList_methods(input_file, SAL);
+
+    SortedLinkedList SLL;
+    measure_SortedList_methods(input_file, SLL);
 }
