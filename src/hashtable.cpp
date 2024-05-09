0f320e81ab48f6f0df7e9a349561cf6e46f89c01
Taikiy49 <taikiy49@gmail.com>
2024-05-08 23:32:12 -0700

    done

--- src/hashtable.cpp
+++ src/hashtable.cpp
@@ -1,131 +1,121 @@
 #include "hashtable.h"
 #include <iostream>
-#include <fstream>
-#include <stack>
-#include <vector>
 #include "Timer.h"
-#include "stats.h"
-#include <string>
+#include <fstream>
 
 using namespace std;
 
-
-// ListNode methods
-ListNode * ListNode::find(string key, ListNode * L) {
-    while (L != nullptr && L->data != key) {
-        L = L->next; // move to the next node
-    }
-    return L; // return the node containing the key
+// LISTNODE -----
+ListNode * ListNode::find(string key, ListNode * L){
+    for (ListNode * p = L; p != nullptr; p=p->next)
+        if (p->data == key) return p;
+    return nullptr;
 }
 
-ListNode * ListNode::insert(string key, ListNode * L) {
-    //if (find(key, L) == nullptr) {
-        L = new ListNode(key, L); // create a new node with the key and add it to the list
-    //}
+ListNode * ListNode::insert(string key, ListNode * L){
+    L = new ListNode(key, L);
     return L;
 }
 
-ListNode * ListNode::remove(string key, ListNode * L) {
-    if (L == nullptr) {
-        return nullptr; // key not found
-    } else if (L->data == key) {
-        ListNode* temp = L->next;// store the next node in a temporary variable
+// this one is a bit complicated. review this later...
+ListNode * ListNode::remove(string key, ListNode* L) {
+    // if the ListNode is empty.
+    if (L == nullptr)
+        return nullptr;
+
+    // if the node that we are trying to remove is the head of the list...
+    if (L->data == key) {
+        ListNode* temp = L->next;
         delete L;
         return temp;
-    } else {
-        L->next = remove(key, L->next);// recursively call remove on the next node
-        return L;// return the current node
     }
-}
 
-void ListNode::print(ostream & out, ListNode * L) {
-    while (L != nullptr) {
-        out << L->data << " ";
-        L = L->next;
-    }
-}
+    ListNode* prev = L;
+    ListNode* current = L->next;
 
-int ListNode::length(ListNode * L) {
-    int count = 0;// initialize a counter 
-    while (L != nullptr) { // while the current node is not null
-        count++;// increment the counter
-        L = L->next;// move to the next node
-    }
-    return count;
-}
-
-void ListNode::delete_list(ListNode * L) {
-    if (L != nullptr) {
-        delete_list(L->next);
-        delete L;
-    }
-}
-
-// HashTable methods
-void HashTable::insert(const string & word
-) {
-    size_t index = hasher.hash(word,capacity);
-    buf[index] = ListNode::insert(word, buf[index]);
-}
-
-bool HashTable::find(const string & word) {
-    size_t index = hasher.hash(word,capacity);
-    return ListNode::find(word, buf[index]);
-}
-
-void HashTable::remove(const string & word) {
-    size_t index = hasher.hash(word,capacity);
-    buf[index] = ListNode::remove(word, buf[index]);
-}
-
-bool HashTable::is_empty() {
-    for (size_t i = 0; i < capacity; ++i) {
-        if (buf[i] != nullptr) {// if not empty
-            return false;
+    while (current != nullptr) {
+        if (current->data == key) {
+            prev->next = current->next;
+            delete current;
+            break;
         }
+        prev = current;
+        current = current->next;
     }
+    return L;
+}
+
+void ListNode::print(ostream & out, ListNode * L){
+    out << L->data << " ";
+}
+
+int ListNode::length(ListNode * L){
+    int i = 0;
+    for (ListNode * p = L; p != nullptr; p=p->next) ++i;
+    return i;
+}
+
+void ListNode::delete_list(ListNode * L){
+    ListNode * l = L;
+    for(;l!=nullptr;){
+        l = l->next;
+        delete L;
+        L = l;
+    }
+}
+
+// HASHTABLE ----- THIS ONE IS A NEW DATA STRUCTURE :o
+void HashTable::insert(const string & word){
+    int h = hasher.hash(word, capacity); // this builds me a hash integer!
+    buf[h] = ListNode::insert(word, buf[h]);
+}
+
+bool HashTable::find(const string & word){
+    int h = hasher.hash(word, capacity);
+    return ListNode::find(word, buf[h]);
+}
+
+void HashTable::remove(const string & word){
+    int h = hasher.hash(word, capacity);
+    buf[h] = ListNode::remove(word, buf[h]);
+}
+
+bool HashTable::is_empty(){
+    for (size_t i = 0; i < capacity; ++i)
+        if (buf[i] != nullptr) return false;
     return true;
 }
 
-bool HashTable::is_full() {
-    return false; // always return false, since the hash table is never full
+bool HashTable::is_full(){ return false; }
+void HashTable::print(ostream & out){
+    for (size_t i = 0; i < capacity; ++i)
+        for (ListNode * head = buf[i]; head != nullptr; head=head->next)
+            ListNode::print(out, head);
 }
-
-void HashTable::print(ostream & out) {
-    for (size_t i = 0; i < capacity; ++i) {
-        ListNode::print(out, buf[i]);
-    }
-}
-
-//destructor
-HashTable::~HashTable() {
-    for (size_t i = 0; i < capacity; ++i) {
+HashTable::~HashTable(){
+    for (size_t i = 0; i < capacity; ++i)
         ListNode::delete_list(buf[i]);
-    }
-    delete[] buf;
+    delete[] buf; // avoid dangling pointers!
 }
 
-//Returns the total number of entries in the hash table.
-size_t HashTable::number_of_entries() {
-    size_t count = 0;// initialize a counter
-    for (size_t i = 0; i < capacity; i++) {
-        // add the length of the list at the current index to the counter
-        count += ListNode::length(buf[i]);
-    }
-    return count;// return the total number of entries
+size_t HashTable::number_of_entries(){
+    size_t count = 0;
+    for (size_t i = 0; i < capacity; ++i)
+        if (buf[i] != nullptr)
+            count += ListNode::length(buf[i]);
+    return count;
 }
 
-size_t HashTable::number_of_chains() {
+size_t HashTable::number_of_chains(){
     return capacity;
 }
 
-void HashTable::get_chain_lengths(vector<int> & v) {
-    for (size_t i = 0; i < capacity; i++) {
-        // push the length of each chain onto the vector
-        v.push_back(ListNode::length(buf[i]));
+void HashTable::get_chain_lengths(vector<int> & v){
+    for (size_t i = 0; i < capacity; ++i){
+        int len = ListNode::length(buf[i]);
+        v.push_back(len);
     }
 }
-
 
 void error(string word, string msg)
 {
