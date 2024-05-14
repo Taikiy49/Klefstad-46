e0d573c7fa23a875b0b6b00749f9ce02dc700627
Taikiy49 <taikiy49@gmail.com>
2024-05-14 12:27:59 -0700

    alomst done

--- src/bst.h
+++ src/bst.h
@@ -42,8 +42,8 @@ struct BST
     static void pre_order_print(ostream & out, Node * t);
     static void in_order_print(ostream & out, Node * t);
     static void post_order_print(ostream & out, Node * t);
-    void free_tree(Node * t);
-
+    static void free_tree(Node * t);
+    
     void print(ostream & out) const
     {
         // pre_order_print(out, root);
