5e336a64dd43361464a086e096b02a02f861858c
Taikiy49 <taikiy49@gmail.com>
2024-05-21 02:42:55 -0700

    done

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,7 @@
 int main()
 {
     // measure_sorters("src/shortrandom.txt");
-    measure_sorters("src/random.txt");
+    // measure_sorters("src/random.txt");
     // measure_sorters("src/shortsorted.txt");
     measure_sorters("src/sorted.txt");
 }
